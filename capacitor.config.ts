db61c9537810c0da5a8d7d789c503ccee0a06fe1
Tomoki Mizogami <tomoki.mizogami@nextbeat.net>
2023-08-01 11:39:48 +0900

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ion-app-capacitor-plugins',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
