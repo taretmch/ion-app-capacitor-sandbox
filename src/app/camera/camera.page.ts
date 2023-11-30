36790b6aec99fd5687d13564d4ccc02b9e60d37c
taretmch <tomoki.mizogami@gmail.com>
2023-11-30 23:50:39 +0900

    Add simple camera feature

--- src/app/camera/camera.page.ts
+++ src/app/camera/camera.page.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {Camera, CameraResultType} from '@capacitor/camera';
 
 @Component({
   selector: 'app-camera',
   templateUrl: './camera.page.html',
   styleUrls: ['./camera.page.scss'],
 })
-export class CameraPage implements OnInit {
+export class CameraPage {
+
+  imgUrl?: string;
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  async takePicture(): Promise<void> {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: true,
+      resultType: CameraResultType.Uri
+    });
 
+    this.imgUrl = image.webPath;
+  }
 }
